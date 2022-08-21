96d64d54d0535070b2bcc0d6fc23265ca27c8eef
lee <1339811657@qq.com>
2022-08-21 19:57:06 +0800

    初始化sql-execution项目

---
+++ sql-execution/src/main/java/com/caiflower/commons/sql/SqlExecutionApplication.java
@@ -0,0 +1,13 @@
+package com.caiflower.commons.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlExecutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlExecutionApplication.class, args);
+    }
+
+}
